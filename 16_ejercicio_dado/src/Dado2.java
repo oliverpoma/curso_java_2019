2518dccd8da60d94c1e0282022c39402d20c2386
ALUMNO <ALUMNO@Aula-Pc>
2019-06-25 09:23:08 +0200

    version2

--- 16_ejercicio_dado/src/Dado2.java
+++ 16_ejercicio_dado/src/Dado2.java
@@ -2,16 +2,16 @@
 public class Dado2 {
 
 	public static void main(String[] args) {
-		int cont[] = new int [6];
+		int contador[] = new int [6];
 		int res=0;
 		
 		for(int i=1;i<=50000;i++) {
 			res=(int)Math.random()*6+1; 			 
-			cont[res-1]++;   //ahorra todo !!!!
+			contador[res-1]++;   //ahorra todo !!!!
 		}
 		
-		for(int i=0;i<cont.length;i++) {
-			System.out.println("El porcenteje de "+(i+1)+" es "+cont[i]/500.0+"%");
+		for(int i=0;i<contador.length;i++) {
+			System.out.println("El porcenteje de "+(i+1)+" es "+contador[i]/500.0+"%");
 		}
 	}
 }
